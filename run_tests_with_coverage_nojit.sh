e19ba127cebeab35dc5e4cf4695422beafb6fff2
Bence Balogh <bencebalogh@sigmaepsilon.com>
2024-09-22 18:25:03 +0200

    updated test scripts

--- run_tests_with_coverage_nojit.sh
+++ run_tests_with_coverage_nojit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export NUMBA_DISABLE_JIT=1
-poetry run pytest --cov-report html --cov-config=.coveragerc_nojit --cov sigmaepsilon.solid.fourier
+poetry run pytest --cov-report=html --cov-config=.coveragerc_nojit --cov=sigmaepsilon.solid.fourier
 export NUMBA_DISABLE_JIT=0
