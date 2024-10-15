183185b7598dfa84fd24de46c4e7e61c756c9af0
Bence Balogh <bencebalogh@sigmaepsilon.com>
2024-10-15 22:44:32 +0200

    added `result_class` and `model_type` to `NavierProblemProtocol`

--- src/sigmaepsilon/solid/fourier/protocols.py
+++ src/sigmaepsilon/solid/fourier/protocols.py
@@ -1,19 +1,24 @@
-from typing import Protocol, runtime_checkable, Iterable, Hashable
+from typing import Protocol, runtime_checkable, Iterable, Hashable, ClassVar
 from numpy import ndarray
 
 from sigmaepsilon.deepdict import DeepDict
 
 from .result import LoadCaseResultLinStat
-
+from .enums import MechanicalModelType
 
 @runtime_checkable
 class NavierProblemProtocol(Protocol):
+    
+    result_class: ClassVar[LoadCaseResultLinStat]
 
     @property
     def size(self) -> Iterable[float | int] | float | int: ...
 
     @property
     def shape(self, value) -> Iterable[int] | int: ...
+    
+    @property
+    def model_type(self) -> MechanicalModelType: ...
 
     def linear_static_analysis(
         self, points, loads
