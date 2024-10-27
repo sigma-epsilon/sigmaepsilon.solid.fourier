969b3df50f259b2151274f3b14795cde35631a21
Bence Balogh <bencebalogh@sigmaepsilon.com>
2024-10-27 13:38:44 +0100

    added new properties to MechanicalModelType class

--- src/sigmaepsilon/solid/fourier/enums.py
+++ src/sigmaepsilon/solid/fourier/enums.py
@@ -7,3 +7,17 @@ class MechanicalModelType(Enum):
     TIMOSHENKO_BEAM = auto()
     UFLYAND_MINDLIN_PLATE = auto()
     KIRCHHOFF_LOVE_PLATE = auto()
+
+    @property
+    def is_1d(self) -> bool:
+        return self in {
+            MechanicalModelType.BERNOULLI_EULER_BEAM,
+            MechanicalModelType.TIMOSHENKO_BEAM,
+        }
+
+    @property
+    def is_2d(self) -> bool:
+        return self in {
+            MechanicalModelType.UFLYAND_MINDLIN_PLATE,
+            MechanicalModelType.KIRCHHOFF_LOVE_PLATE,
+        }
