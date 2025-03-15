39db09c1a2a3c557deefc73cade617121a82ad9e
Bence Balogh <bence.balogh@datapao.com>
2025-03-15 21:58:56 +0100

    fix: implement method bodies for protocols in fourier module

--- src/sigmaepsilon/solid/fourier/protocols.py
+++ src/sigmaepsilon/solid/fourier/protocols.py
@@ -13,44 +13,55 @@ class NavierProblemProtocol(Protocol):
     result_class: ClassVar[LoadCaseResultLinStat]
 
     @property
-    def size(self) -> Iterable[float | int] | float | int: ...
+    def size(self) -> Iterable[float | int] | float | int:
+        pass
 
     @property
-    def shape(self, value) -> Iterable[int] | int: ...
+    def shape(self, value) -> Iterable[int] | int:
+        pass
 
     @property
-    def model_type(self) -> MechanicalModelType: ...
+    def model_type(self) -> MechanicalModelType:
+        pass
 
     def linear_static_analysis(
         self, points, loads
-    ) -> DeepDict[Hashable, DeepDict | LoadCaseResultLinStat]: ...
+    ) -> DeepDict[Hashable, DeepDict | LoadCaseResultLinStat]:
+        pass
 
 
 @runtime_checkable
 class LoadCaseProtocol(Protocol):
     
     @property
-    def domain(self): ...
+    def domain(self):
+        pass
 
     @domain.setter
-    def domain(self, value): ...
+    def domain(self, value):
+        pass
 
     @property
-    def value(self): ...
+    def value(self):
+        pass
 
     @value.setter
-    def value(self, value): ...
+    def value(self, value):
+        pass
 
     def eval_approx(self, problem: NavierProblemProtocol, points: Iterable) -> ndarray:
         """Evaluates the Fourier series approximation of the load at the given points."""
-        ...
+        pass
 
     def rhs(self, problem: NavierProblemProtocol) -> ndarray:
         """Calculates the Fourier coefficients."""
-        ...
+        pass
 
 
 @runtime_checkable
 class LoadGroupProtocol(Protocol):
-    def groups(self, *args, **kwargs) -> Iterable["LoadGroupProtocol"]: ...
-    def cases(self, *args, **kwargs) -> Iterable[LoadCaseProtocol]: ...
+    def groups(self, *args, **kwargs) -> Iterable["LoadGroupProtocol"]:
+        pass
+    
+    def cases(self, *args, **kwargs) -> Iterable[LoadCaseProtocol]:
+        pass
